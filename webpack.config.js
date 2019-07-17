d4d8c26d6da146a4d85c75701821ed16a31e3575
Weismann <1563511027@qq.com>
2019-07-18 00:25:31 +0800

    fix bug and update electron version

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         filename: '[name].bundle.js', //编译后的文件
         path: path.resolve(__dirname, 'dist')
     },
+    mode: 'development',
     module: {
         rules: [
             {
